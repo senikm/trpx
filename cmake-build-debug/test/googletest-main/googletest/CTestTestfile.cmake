e937da5e3fae8612ca5680205e3a4b8bdbb507f4
Senik Matinyan <matiny0000@bz-rgja01-plm07.bioz.unibas.ch>
2023-07-24 22:05:12 +0200

    Aesthetic flows have been improved

--- cmake-build-debug/test/googletest-main/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/matiny0000/Documents/PhD/terse/test/googletest-main/googletest
-# Build directory: /Users/matiny0000/Documents/PhD/terse/cmake-build-debug/test/googletest-main/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
